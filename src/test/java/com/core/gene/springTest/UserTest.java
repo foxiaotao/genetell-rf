1d7ca3f03ed8cf5d7d44be29caa0cf2b0f32b92b
simon <384631173@qq.com>
2017-03-30 14:28:18 +0800

    1
    1

--- src/test/java/com/core/gene/springTest/UserTest.java
+++ src/test/java/com/core/gene/springTest/UserTest.java
@@ -26,8 +26,8 @@ public class UserTest extends JunitSpringTest{
 	
 	@Test
 	public void indextest(){
-		
-		List<User> list = userServiceImpl.selectPageByEntity(new User(), 0, 10);
+		//我update	
+ 		List<User> list = userServiceImpl.selectPageByEntity(new User(), 0, 10);
 		System.out.println(list.size());
 	}
 	
